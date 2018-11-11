f604eb191e62643a0026ef414368fc0bc5aed5e2
Eric Castillo <ecastillo11@gmail.com>
2018-11-11 11:45:09 -0600

    Initial Commit

---
+++ SwiftSonarr/SwiftSonarr.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSonarr.h
+//  SwiftSonarr
+//
+//  Created by Eric Castillo on 11/11/18.
+//  Copyright © 2018 Eric Castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSonarr.
+FOUNDATION_EXPORT double SwiftSonarrVersionNumber;
+
+//! Project version string for SwiftSonarr.
+FOUNDATION_EXPORT const unsigned char SwiftSonarrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSonarr/PublicHeader.h>
+
+
